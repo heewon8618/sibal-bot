91667f490136b9b84b732bee8c2148a9f28b5019
heewon8618 <69801502+heewon8618@users.noreply.github.com>
2022-01-06 23:34:45 +0900

    Add files via upload

---
+++ commands/stop.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "dis",
+  run: (message) => { 
+    const player = message.client.manager.get(message.guild.id);
+    if (!player) return message.reply("there is no player for this guild.");
+
+    const { channel } = message.member.voice;
+    
+    if (!channel) return message.reply("you need to join a voice channel.");
+    if (channel.id !== player.voiceChannel) return message.reply("you're not in the same voice channel.");
+    
+    player.destroy();
+    return message.reply("간다 병신들아 ㅋ");
+  }
+}
